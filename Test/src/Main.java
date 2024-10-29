72a500714e54c32e05d2d201fc5f814cfd8005eb
Reemas Alyahia <130400514+mime2001@users.noreply.github.com>
2024-10-29 10:51:20 +0300

    Add files via upload

---
+++ Test/src/Main.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        //Write a Java program that accept three numbers from the user and print the largest number .
+//        System.out.println("enter 3 number :");
+        Scanner input=new Scanner(System.in);
+//        System.out.println("first number");
+//        int n1=input.nextInt();
+//        System.out.println("second number");
+//        int n2=input.nextInt();
+//        System.out.println("therd number");
+//        int n3=input.nextInt();
+//        if(n1>n2&&n1>n3){
+//            System.out.println("the first number is the larger");
+//        }
+//        if(n2>n1&&n2>n3){
+//            System.out.println("the second number is the larger");
+//        } else System.out.println("the therd one is the larger");
+        System.out.println("---------------------------");
+
+//        Write a Java program that accept a String and a number from the user,
+//        then print the character in the given index .
+//        System.out.println("enter word");
+//        String w=input.nextLine();
+//        System.out.println("enter number");
+//        int n=input.nextInt();
+//        System.out.println("th full word is : "+w.charAt(n));
+        System.out.println("---------------------------");
+
+        ///////////////////////////////////////////////
+//        Write a program to enter the numbers till the user wants and
+//        at the end it should display the sum entered .
+
+//        System.out.println("how many number we well sum?");
+//        int cou=input.nextInt();
+//        int sum=0;
+//        for (int i = 0; i <cou ; i++) {
+//            System.out.println("enter number: ");
+//            int num=input.nextInt();
+//            sum=sum+num;
+//        }
+//        System.out.println("the sum is :"+sum);
+        ////////////////////////////////
+        System.out.println("---------------------------");
+
+        //Write a Java program to find positive and negative numbers of a given array:
+//        int [] num={10, -21 , 30, 31, -25};
+//        for(int n:num){
+//            if(n>0){
+//                System.out.println(n+" the number is positive");
+//            }
+//            else if (n<0) {
+//                System.out.println(n+" the number is negative");
+//
+//            }
+//        }
+        System.out.println("---------------------------");
+        //Write a Java program to find a shortest word of a given array
+       // String[] name={"Tuwaiq", "Bootcamp" , ,"JAVA"};
+//        ArrayList <String>  name=new ArrayList<>();
+//        ArrayList <String> nn=new ArrayList<>();
+//
+//        name.add("Bootcamp"); name.add("Tuwaiq"); name.add("Student");  name.add("JAVA");
+//        System.out.println("Enter your limet :) ");
+//int min=input.nextInt();
+//        for (String n:name){
+//            if(n.length()<=min){
+//            nn.add(n);}
+//            }System.out.println(nn);
+
+
+
+
+}}
